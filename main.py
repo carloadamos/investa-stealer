659260e6f7a9308bc2e45aecb554b84f165bcea8
carloadamos <johncarloadamos@gmail.com>
2020-11-08 01:08:43 +0800

    get, save stocks

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import time
 import pandas as pd
 import math
 from AlmaIndicator import ALMAIndicator
-from constants.stocklist import test_list
+from constants.stocklist import stock_list
+
+# Database
+import pymongo
+from flask import Flask
+from flask_cors import CORS
 
 # Start of test - 20160101
 ots = '1451581261'
@@ -14,24 +19,64 @@ cts = calendar.timegm(time.gmtime())
 volumeArray = []
 valueArray = []
 
-def try_panda(list):
+
+def compute_indicators(list):
 		df = pd.DataFrame(list)
-		df.columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'value']
-		df['MA20'] = df.close.rolling(20).mean()
-		df['MA50'] = df.close.rolling(50).mean()
-		df['MA100'] = df.close.rolling(100).mean()
+		df.columns = ['code', 'timestamp', 'open',
+									'high', 'low', 'close', 'volume', 'value']
 		alma_indicator = ALMAIndicator(close=df['close'])
+
+		df['ma20'] = df.close.rolling(20).mean()
+		df['ma50'] = df.close.rolling(50).mean()
+		df['ma100'] = df.close.rolling(100).mean()
 		df['alma'] = alma_indicator.alma()
-		print(df.tail(30))
- 
-for stock in test_list:
+
+		result = df.to_json(orient="records")
+		parsed = json.loads(result)
+		# temporary solution
+		# should have gui to trigger delete
+		# should also implement upsert
+		saveStocks(parsed)
+
+
+connection_url = 'mongodb+srv://admin:admin@cluster0.70gug.mongodb.net/exercise-tracker?retryWrites=true&w=majority'
+app = Flask(__name__)
+client = pymongo.MongoClient(connection_url)
+
+# Database
+Database = client.get_database('stock-analyzer')
+# Table
+stocks_table = Database.stocks
+
+
+def saveStocks(stocks):
+		stocks_table.insert_many(stocks)
+
+
+def deleteAllStocks():
+		stocks_table.delete_many({})
+
+
+# deleteAllStocks()
+for stock in stock_list:
 		i = 0
 		j = 0
+		ohlctValue = []
+		ohlctVolume = []
+		volumeArray = []
+		valueArray = []
+
 		volumeApi = 'https://api.kisschart.com/api/chart/history?symbol={0}&resolution=D&from={1}&to={2}&firstDataRequest=true'
 		valueApi = 'https://api.kisschart.com/api/chart/history?symbol={0}%23VALUE&resolution=D&from={1}&to={2}&firstDataRequest=true'
 
 		# OHLC, timestamp and Volume
-		ohlcVolume = requests.get(volumeApi.format(stock, ots, cts))
+		print('calling', volumeApi.format(stock, ots, cts))
+
+		try:
+				ohlcVolume = requests.get(volumeApi.format(stock, ots, cts))
+		except:
+				print('Error on getting volume for ', stock)
+
 		jsonVolume = ohlcVolume.content.decode('utf8').replace("'", '"')
 		volumeJson = json.loads(jsonVolume)
 
@@ -44,7 +89,13 @@ for stock in test_list:
 		volume_time_stamp = volumeJson['t']
 
 		# OHLC, timestamp and Value
-		ohlcValue = requests.get(valueApi.format(stock, ots, cts))
+		print('calling', valueApi.format(stock, ots, cts))
+
+		try:
+				ohlcValue = requests.get(valueApi.format(stock, ots, cts))
+		except:
+				print('Error on getting value for ', stock)
+
 		jsonValue = ohlcValue.content.decode('utf8').replace("'", '"')
 		valueJson = json.loads(jsonValue)
 
@@ -54,7 +105,7 @@ for stock in test_list:
 
 		# Volume Loop
 		for time in volume_time_stamp:
-				ohlctVolume = [time, openPrice[i], high[i],
+				ohlctVolume = [stock, time, openPrice[i], high[i],
 											 low[i], close[i], volume[i]]
 				volumeArray.append(ohlctVolume)
 				i = i + 1
@@ -67,7 +118,7 @@ for stock in test_list:
 
 		for vol in volumeArray:
 				for val in valueArray:
-						if vol[0] == val[0]:
+						if vol[1] == val[0]:
 								vol.append(val[1])
 
-		try_panda(volumeArray)
+		compute_indicators(volumeArray)
